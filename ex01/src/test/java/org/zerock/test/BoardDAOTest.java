b25c9937e3ae03e3e5c56e2b7c9a3bdd2dc6e056
noCountJun <ansejjang@gmail.com>
2020-01-17 23:54:31 +0900

    service regist

--- ex01/src/test/java/org/zerock/test/BoardDAOTest.java
+++ ex01/src/test/java/org/zerock/test/BoardDAOTest.java
@@ -1,5 +1,41 @@
 package org.zerock.test;
 
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.persistence.BoardDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations ={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class BoardDAOTest {
 
+	@Inject
+	private BoardDAO dao;
+	
+	private static Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
+
+	@Test
+	public void testCreate() throws Exception {
+		BoardVO board = new BoardVO();
+		board.setTitle("신규글");
+		board.setContent("새로운글 내용");
+		board.setWriter("user00");
+		dao.create(board);
+			
+	}
+	
+	@Test
+	public void testUpdate() throws Exception {
+		BoardVO board = new BoardVO();
+		board.setBno(1);
+		board.setTitle("수정타이틀");
+		board.setContent("수정 컨텐츠");
+		dao.update(board);
+	}
 }
